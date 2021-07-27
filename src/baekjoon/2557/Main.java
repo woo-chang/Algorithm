0e8c7856a74fb0f6fa8ae17844dc88602aef1330
choiwoochang <clllickme@naver.com>
2021-07-27 19:18:31 +0900

    Baekjoon: No.2557

---
+++ src/baekjoon/2557/Main.java
@@ -0,0 +1,12 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw.write("Hello World!\n");
+		bw.flush();
+	}
+}
