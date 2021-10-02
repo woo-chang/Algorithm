8534e72a80e7b68c8a46bb05e6bd51a3e05b9696
choiwoochang <clllickme@naver.com>
2021-10-02 17:20:26 +0900

    Baekjoon: No.1309

---
+++ src/baekjoon/1309/1309.py
@@ -0,0 +1,14 @@
+def main():
+    dp = [[0, 0] for _ in range(100001)]
+    n = int(input())
+    dp[1][0] = 1
+    dp[1][1] = 2
+    for i in range(2, n+1):
+        dp[i][0] = dp[i-1][0] + dp[i-1][1]
+        dp[i][1] = dp[i-1][0]*2 + dp[i-1][1]
+        dp[i][0] %= 9901
+        dp[i][1] %= 9901
+    print((dp[n][0]+dp[n][1]) % 9901)
+
+
+main()
