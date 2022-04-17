46594b173066fc94b8e023117fdec0f9a967a85e
최우창 <clllickme@naver.com>
2022-04-17 17:19:56 +0900

    feat: 다익스트라 알고리즘

---
+++ Theory/ShortestPath/Edge.java
@@ -0,0 +1,18 @@
+public class Edge implements Comparable<Edge> {
+    public int distance;
+    public String vertex;
+
+    public Edge(int distance, String vertex) {
+        this.distance = distance;
+        this.vertex = vertex;
+    }
+
+    public String toString() {
+        return "vertex: " + this.vertex + ", distance: " + this.distance;
+    }
+
+    @Override
+    public int compareTo(Edge edge) {
+        return this.distance - edge.distance;
+    }
+}
