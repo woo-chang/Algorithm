053d27f9a126e685c7652e6f746be12e2868ffd9
woo-chang <clllickme@naver.com>
2022-06-28 15:01:53 +0900

    Programmers: 같은 숫자는 싫어

---
+++ src/programmers/Level1/같은 숫자는 싫어/solution.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        int[] answer = {};
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(arr[0]);
+        
+        for (int i = 1; i < arr.length; i++) {
+            if (list.get(list.size()-1) != arr[i]) list.add(arr[i]);
+        }
+
+        return list.stream().mapToInt(i->i).toArray();
+    }
+}
